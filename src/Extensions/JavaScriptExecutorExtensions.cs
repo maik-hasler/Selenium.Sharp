00247a5cd36359bada94159d3ddf32152a81571f
maik-hasler <maikhslr@gmail.com>
2023-04-18 10:33:59 +0200

    Added JavaScript extensions

---
+++ src/Extensions/JavaScriptExecutorExtensions.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace Selenium.Sharp.Extensions;
+
+internal static class JavaScriptExecutorExtensions
+{
+    public static void Click(this IJavaScriptExecutor executor, IWebElement elementToBeClicked)
+    {
+        executor.ExecuteScript("arguments[0].click();", elementToBeClicked);
+    }
+}
---
+++ src/Extensions/WebDriverExtensions.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace Selenium.Sharp.Extensions;
+
+internal static class WebDriverExtensions
+{
+    public static IJavaScriptExecutor Scripts(this IWebDriver webDriver)
+    {
+        return (IJavaScriptExecutor)webDriver;
+    }
+}
