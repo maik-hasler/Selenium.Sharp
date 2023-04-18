83e68e088c99d5ae2b192c2946f43c37b4db47b9
maik-hasler <maikhslr@gmail.com>
2023-04-18 10:21:56 +0200

    Added XML documentation

--- src/WaitConditions.cs
+++ src/WaitConditions.cs
@@ -2,13 +2,26 @@
 
 namespace Selenium.Sharp;
 
+/// <summary>
+/// Provides a collection of commonly used selenium wait conditions.
+/// </summary>
 public static class WaitConditions
 {
+    /// <summary>
+    /// Returns a function that checks if an element with the specified locator exists on the page.
+    /// </summary>
+    /// <param name="by">The locator of the element to search for.</param>
+    /// <returns>A function that takes an ISearchContext and returns an IWebElement.</returns>
     public static Func<ISearchContext, IWebElement> ElementExists(By by)
     {
         return (searchContext) => searchContext.FindElement(by);
     }
 
+    /// <summary>
+    /// Returns a function that checks if an element with the specified locator is visible on the page.
+    /// </summary>
+    /// <param name="by">The locator of the element to search for.</param>
+    /// <returns>A function that takes an ISearchContext and returns a bool indicating if the element is visible.</returns>
     public static Func<ISearchContext, bool> ElementIsVisible(By by)
     {
         return (searchContext) =>
