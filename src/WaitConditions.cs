83704165ee71931cc57bedcd6ee8d091da69149b
maik-hasler <maikhslr@gmail.com>
2023-04-18 10:20:00 +0200

    Added more wait conditions

--- src/WaitConditions.cs
+++ src/WaitConditions.cs
@@ -4,8 +4,18 @@ namespace Selenium.Sharp;
 
 public static class WaitConditions
 {
-    public static Func<ISearchContext, IWebElement> ElementExists(By locator)
+    public static Func<ISearchContext, IWebElement> ElementExists(By by)
     {
-        return (searchContext) => searchContext.FindElement(locator);
+        return (searchContext) => searchContext.FindElement(by);
+    }
+
+    public static Func<ISearchContext, bool> ElementIsVisible(By by)
+    {
+        return (searchContext) =>
+        {
+            var element = searchContext.FindElement(by);
+
+            return element.Displayed;
+        };
     }
 }
--- tests/WaitConditionsTests.cs
+++ tests/WaitConditionsTests.cs
@@ -45,4 +45,52 @@ public class WaitConditionsTests
         // Assert
         act.Should().Throw<NoSuchElementException>();
     }
+
+    [Fact]
+    public void ElementIsVisible_ReturnsTrue_WhenElementIsDisplayed()
+    {
+        // Arrange
+        var fixture = new Fixture().Customize(new AutoMoqCustomization());
+        var expectedElement = fixture.Create<Mock<IWebElement>>();
+        expectedElement.Setup(x => x.Displayed).Returns(true);
+        var locator = By.Id("test");
+        _searchContextMock.Setup(x => x.FindElement(locator)).Returns(expectedElement.Object);
+
+        // Act
+        var result = WaitConditions.ElementIsVisible(locator).Invoke(_searchContextMock.Object);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ElementIsVisible_ReturnsFalse_WhenElementIsNotDisplayed()
+    {
+        // Arrange
+        var fixture = new Fixture().Customize(new AutoMoqCustomization());
+        var expectedElement = fixture.Create<Mock<IWebElement>>();
+        expectedElement.Setup(x => x.Displayed).Returns(false);
+        var locator = By.Id("test");
+        _searchContextMock.Setup(x => x.FindElement(locator)).Returns(expectedElement.Object);
+
+        // Act
+        var result = WaitConditions.ElementIsVisible(locator).Invoke(_searchContextMock.Object);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ElementIsVisible_ThrowsNoSuchElementException_WhenElementIsNotFound()
+    {
+        // Arrange
+        var locator = By.Id("test");
+        _searchContextMock.Setup(x => x.FindElement(locator)).Throws(new NoSuchElementException());
+
+        // Act
+        Action act = () => WaitConditions.ElementIsVisible(locator).Invoke(_searchContextMock.Object);
+
+        // Assert
+        act.Should().Throw<NoSuchElementException>();
+    }
 }
