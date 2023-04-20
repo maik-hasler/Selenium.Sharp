803518b49ebc3e8345af3ea697e652e068ac5115
maik-hasler <maikhslr@gmail.com>
2023-04-20 09:37:55 +0200

    Added extension method for dispatching DOM events

--- src/JavaScriptExecutorExtensions.cs
+++ src/JavaScriptExecutorExtensions.cs
@@ -8,4 +8,11 @@ public static class JavaScriptExecutorExtensions
     {
         executor.ExecuteScript("arguments[0].click();", elementToBeClicked);
     }
+
+    public static void DispatchEvent(this IJavaScriptExecutor executor, IWebElement webElement, DomEvent domEvent)
+    {
+        var domEventName = domEvent.ToString().ToLower();
+
+        executor.ExecuteScript("[0].dispatchEvent(new Event('[1]'));", webElement, domEventName);
+    }
 }
