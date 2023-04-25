b993732ea228814c01fc05288fc615d12f64f7f6
maik-hasler <maikhslr@gmail.com>
2023-04-25 09:22:33 +0200

    Added XML documentation

--- src/Managers/WebDriverManager.cs
+++ src/Managers/WebDriverManager.cs
@@ -3,8 +3,18 @@ using SeleniumSharper.Managers.Interfaces;
 
 namespace SeleniumSharper.Managers;
 
+/// <summary>
+/// Provides a simple way to create an instance of an <see cref="IWebDriverManager{T}"/> for a specific <see cref="IWebDriver"/> type.
+/// </summary>
 public static class WebDriverManager
 {
+    /// <summary>
+    /// Creates an instance of an <see cref="IWebDriverManager{T}"/> for the specified <see cref="IWebDriver"/> type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The <see cref="IWebDriver"/> type to create an instance of.</typeparam>
+    /// <returns>An instance of an <see cref="IWebDriverManager{T}"/>.</returns>
+    /// <exception cref="NotSupportedException">Thrown when the specified <see cref="IWebDriver"/> type is not supported.</exception>
+    /// <exception cref="NullReferenceException">Thrown when the instance of the <see cref="IWebDriverManager{T}"/> could not be created.</exception>
     public static IWebDriverManager<T> For<T>()
         where T : IWebDriver
     {
